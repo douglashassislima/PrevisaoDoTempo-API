561e6fcf3c987bf7633a795bd69582fd3bae7cc0
douglashassislima <douglashenriquedeassis@gmail.com>
2021-03-28 22:55:09 -0300

    projeto inicial finalizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+import * as moment from 'moment-timezone';
+import 'moment/locale/pt-br';
+
 @Component({
-  selector: 'spa-root',
+  selector: 'jv-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'previsadotempo-api';
+
+  constructor() {
+    moment.locale('pt-br');
+  }
 }
